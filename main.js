3ac79dbff9e7c38ae6db72e32b733922d58738a1
daniel <prokopowiczdaniel@gmail.com>
2019-06-16 12:43:24 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+//alert('siema');
+console.log('siema, jak masz na imię?');
+const imie="Daniel";
+let wiek=39;
+console.log("Mam na imię "+imie+" i mam "+wiek+" lat");
+console.log(`Mam na imię ${imie} i mam ${wiek} lat`);
+console.log(`Mam na imię ${imie} 
+i mam ${wiek} lat`);
