7fc858dc508e6351ebc2936bbd158bf3470deddd
daniel <prokopowiczdaniel@gmail.com>
2019-08-04 16:34:09 +0200

    hambuger menu

--- main.js
+++ main.js
@@ -119,5 +119,12 @@ button.addEventListener('click', (e) => {
     }
 
     document.querySelector('.mainheader--js').innerHTML = zamiana;
-    
+    document.querySelector('.mainheader--js').classList.toggle('mainheader--red');
+});
+let visible__menu=document.querySelector('.navigation__list--js')
+let swicher=document.querySelector('.navigation__swicher--js');
+let opcja1=swicher.innerHTML;
+swicher.addEventListener('click',() =>{visible__menu.classList.toggle('nawigacja__lista--modyfikacja');
+if(swicher.innerHTML=='x')swicher.innerHTML=opcja1;
+else swicher.innerHTML='x';
 });
